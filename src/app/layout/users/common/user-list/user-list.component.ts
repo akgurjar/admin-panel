0e22b51bf1791090c875874491d69c1af5811d75
Ashish Gurjar <akp51081@gmail.com>
2018-12-11 02:20:44 +0530

    ...

--- src/app/layout/users/common/user-list/user-list.component.ts
+++ src/app/layout/users/common/user-list/user-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LIST_CONFIG, UserTableSource } from './user-list.model';
 import { UserListService } from './user-list.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,7 +12,7 @@ import { UserListService } from './user-list.service';
 export class UserListComponent implements OnInit {
   listingConfig: Listing.Config = LIST_CONFIG;
   tableSource: Table.Source<any> = new UserTableSource([]);
-  constructor(private _userList: UserListService) {
+  constructor(private _userList: UserListService, private _router: Router) {
     _userList.changes.subscribe((data: any[]) => {
       this.tableSource = new UserTableSource(data);
     });
@@ -19,5 +20,7 @@ export class UserListComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  onCreateUser() {
+    this._router.navigateByUrl('/users/create');
+  }
 }
